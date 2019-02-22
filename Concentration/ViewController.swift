11598ed0c85cc5850decd881b7851ee06c51f7ca
Rechargeable22 <oliver.leuschner@gmx.de>
2019-02-22 21:59:28 +0100

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Oliver on 22.02.19.
+//  Copyright © 2019 OliCorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
